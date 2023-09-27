dfb2e4ef5824ced274eb536482a35060fda95d7b
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-09-27 19:12:24 -0400

    Database seeding

--- src/main/java/com/lighthousecorp/clientcrud/entities/Client.java
+++ src/main/java/com/lighthousecorp/clientcrud/entities/Client.java
@@ -12,6 +12,7 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique = true)
     private String cpf;
     private Double income;
     private LocalDate birthDate;
