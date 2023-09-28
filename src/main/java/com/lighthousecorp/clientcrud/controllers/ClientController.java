2cc5d659c347ae73184561bd197af25f8dfaa4b1
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-09-28 05:12:00 -0400

    POST Client

--- src/main/java/com/lighthousecorp/clientcrud/controllers/ClientController.java
+++ src/main/java/com/lighthousecorp/clientcrud/controllers/ClientController.java
@@ -5,12 +5,7 @@ import com.lighthousecorp.clientcrud.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -28,5 +23,10 @@ public class ClientController {
     @GetMapping
     public Page<ClientDTO> findAll(Pageable pageable) {
         return service.findAll(pageable);
+    }
+
+    @PostMapping
+    public ClientDTO insert(@RequestBody ClientDTO dto) {
+        return service.insert(dto);
     }
 }
