80341b8b413e5f3c70260d9f121f80f218e411e5
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-09-28 03:44:21 -0400

    GET Products paged

--- src/main/java/com/lighthousecorp/clientcrud/services/ClientService.java
+++ src/main/java/com/lighthousecorp/clientcrud/services/ClientService.java
@@ -4,10 +4,13 @@ import com.lighthousecorp.clientcrud.dto.ClientDTO;
 import com.lighthousecorp.clientcrud.entities.Client;
 import com.lighthousecorp.clientcrud.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
+
 
 @Service
 public class ClientService {
@@ -19,5 +22,11 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client client  = repository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result  = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
