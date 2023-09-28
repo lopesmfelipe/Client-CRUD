abf48b0067bf5dad37c05a605d5d57c606118936
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-09-27 20:18:39 -0400

    First test with Repository

--- src/main/java/com/lighthousecorp/clientcrud/controllers/ClientController.java
+++ src/main/java/com/lighthousecorp/clientcrud/controllers/ClientController.java
@@ -1,15 +1,25 @@
 package com.lighthousecorp.clientcrud.controllers;
 
+import com.lighthousecorp.clientcrud.entities.Client;
+import com.lighthousecorp.clientcrud.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientController {
 
+    @Autowired
+    private ClientRepository repository;
+
     @GetMapping
     public String test() {
-        return "The world is yours!";
+        Optional<Client> result = repository.findById(1L);
+        Client client = result.get();
+        return client.getName();
     }
 }
